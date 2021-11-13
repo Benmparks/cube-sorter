69bdc4d2c91742f8abfc8ea87123399ee9d3c606
Benmparks <93792887+Benmparks@users.noreply.github.com>
2021-11-13 02:38:35 -0500

    Lots of changes, the biggest one being that all commons/uncommons are now unique

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {useEffect,useState} from 'react';
 import './style.css';
-	
-var sessionStorage = window.sessionStorage;
+
+window.onload = function(){
+	sessionStorage.clear();
+}
 
 function makePools(setData) {
 	
@@ -51,11 +53,17 @@ function makePools(setData) {
 	
 	//Save info about the boosters in the session so we have access to it later
 	const boosterData = setData.data.booster.default.boosters[0].contents;
-	sessionStorage.setItem('numCommons', boosterData.sfcCommon);
-	sessionStorage.setItem('numUnCommons', boosterData.sfcUncommon);
-	sessionStorage.setItem('numRareMythic', boosterData.sfcRareMythic);
-	sessionStorage.setItem('numDFC', boosterData.dfc);
-
+	if(boosterData.dfc != null) {
+		sessionStorage.setItem('numCommons', boosterData.sfcCommon);
+		sessionStorage.setItem('numUnCommons', boosterData.sfcUncommon);
+		sessionStorage.setItem('numRareMythic', boosterData.sfcRareMythic);
+		sessionStorage.setItem('numDFC', boosterData.dfc);
+	} else {
+		sessionStorage.setItem('numCommons', boosterData.common);
+		sessionStorage.setItem('numUnCommons', boosterData.uncommon);
+		sessionStorage.setItem('numRareMythic', boosterData.rareMythic);
+		sessionStorage.setItem('numDFC', null);
+	}
 		
 	for(let i = 0; i < cardSet.length -1; i++) {
 		
@@ -103,94 +111,202 @@ function makePools(setData) {
 			}
 		}
 	}
-	
-	sessionStorage.setItem('cardPoolSFCCommon', JSON.stringify(cardPoolSFCCommon));
-	sessionStorage.setItem('cardPoolSFCUnCommon', JSON.stringify(cardPoolSFCUnCommon));
-	sessionStorage.setItem('cardPoolSFCRareMythic', JSON.stringify(cardPoolSFCRareMythic));
-	sessionStorage.setItem('cardPoolDFC', JSON.stringify(cardPoolDFC));
+	if(boosterData.dfc != null) {
+		sessionStorage.setItem('cardPoolSFCCommon', JSON.stringify(cardPoolSFCCommon));
+		sessionStorage.setItem('cardPoolSFCUnCommon', JSON.stringify(cardPoolSFCUnCommon));
+		sessionStorage.setItem('cardPoolSFCRareMythic', JSON.stringify(cardPoolSFCRareMythic));
+		sessionStorage.setItem('cardPoolDFC', JSON.stringify(cardPoolDFC));
+		sessionStorage.setItem('originalCardPoolSFCCommon', JSON.stringify(cardPoolSFCCommon));
+		sessionStorage.setItem('originalCardPoolSFCUnCommon', JSON.stringify(cardPoolSFCUnCommon));
+		sessionStorage.setItem('originalCardPoolSFCRareMythic', JSON.stringify(cardPoolSFCRareMythic));
+		sessionStorage.setItem('originalCardPoolDFC', JSON.stringify(cardPoolDFC));
+	} else {
+		sessionStorage.setItem('cardPoolSFCCommon', JSON.stringify(cardPoolSFCCommon));
+		sessionStorage.setItem('cardPoolSFCUnCommon', JSON.stringify(cardPoolSFCUnCommon));
+		sessionStorage.setItem('cardPoolSFCRareMythic', JSON.stringify(cardPoolSFCRareMythic));
+		sessionStorage.setItem('cardPoolDFC', null);
+		sessionStorage.setItem('originalCardPoolSFCCommon', JSON.stringify(cardPoolSFCCommon));
+		sessionStorage.setItem('originalCardPoolSFCUnCommon', JSON.stringify(cardPoolSFCUnCommon));
+		sessionStorage.setItem('originalCardPoolSFCRareMythic', JSON.stringify(cardPoolSFCRareMythic));
+		sessionStorage.setItem('originalCardPoolDFC', null);
+	}
 }
 
-function makePacks(num) {	
-	//Get the data from session storage
-	let cardPoolSFCCommon = JSON.parse(sessionStorage.getItem('cardPoolSFCCommon'));
-	let cardPoolSFCUnCommon = JSON.parse(sessionStorage.getItem('cardPoolSFCUnCommon'));
-	let cardPoolSFCRareMythic = JSON.parse(sessionStorage.getItem('cardPoolSFCRareMythic'));
-	let cardPoolDFC = JSON.parse(sessionStorage.getItem('cardPoolDFC'));
+function determineMaxPacks(isReset) {
+	
+	let cardPoolSFCCommon = [];
+	let cardPoolSFCUnCommon = [];
+	let cardPoolSFCRareMythic = [];
+	let cardPoolDFC = [];
 	let numCommons = sessionStorage.getItem('numCommons');
 	let numUnCommons = sessionStorage.getItem('numUnCommons');
 	let numRareMythic = sessionStorage.getItem('numRareMythic');
 	let numDFC = sessionStorage.getItem('numDFC');
-		
-	if(sessionStorage.getItem('box') === null) {
-		var box = [];
+	let maxPacksArr = [];
+	let maxPacks = 0;
+
+	
+	if(isReset) {
+		cardPoolSFCCommon = JSON.parse(sessionStorage.getItem('originalCardPoolSFCCommon'));
+		cardPoolSFCUnCommon = JSON.parse(sessionStorage.getItem('originalCardPoolSFCUnCommon'));
+		cardPoolSFCRareMythic = JSON.parse(sessionStorage.getItem('originalCardPoolSFCRareMythic'));
+		cardPoolDFC = JSON.parse(sessionStorage.getItem('originalCardPoolDFC'));
 	} else {
-		var box = JSON.parse(sessionStorage.getItem('box'));
+		cardPoolSFCCommon = JSON.parse(sessionStorage.getItem('cardPoolSFCCommon'));
+		cardPoolSFCUnCommon = JSON.parse(sessionStorage.getItem('cardPoolSFCUnCommon'));
+		cardPoolSFCRareMythic = JSON.parse(sessionStorage.getItem('cardPoolSFCRareMythic'));
+		cardPoolDFC = JSON.parse(sessionStorage.getItem('cardPoolDFC'));
 	}
 	
-	//How many packs to make in the box
-	const numPacks = num;
+	maxPacksArr.push(Math.trunc(cardPoolSFCCommon.length/numCommons));
+	maxPacksArr.push(Math.trunc(cardPoolSFCUnCommon.length/numUnCommons));
+	maxPacksArr.push(Math.trunc(cardPoolSFCRareMythic.length/numRareMythic));
+	if(numDFC === null) maxPacksArr.push(Math.trunc(cardPoolDFC.length/numDFC));
+	maxPacks = Math.min.apply(Math, maxPacksArr);
+	sessionStorage.setItem('maxPacksOriginal', JSON.stringify(maxPacks));
+	sessionStorage.setItem('maxPacksCounter', JSON.stringify(maxPacks));
+}
+
+function makePacks(num, isReset) {	
+	//Get the data from session storage
+	let cardPoolSFCCommon = [];
+	let cardPoolSFCUnCommon = [];
+	let cardPoolSFCRareMythic = [];
+	let cardPoolDFC = [];
+	
+	if(isReset) {
+		cardPoolSFCCommon = JSON.parse(sessionStorage.getItem('originalCardPoolSFCCommon'));
+		cardPoolSFCUnCommon = JSON.parse(sessionStorage.getItem('originalCardPoolSFCUnCommon'));
+		cardPoolSFCRareMythic = JSON.parse(sessionStorage.getItem('originalCardPoolSFCRareMythic'));
+		cardPoolDFC = JSON.parse(sessionStorage.getItem('originalCardPoolDFC'));
+	} else {
+		cardPoolSFCCommon = JSON.parse(sessionStorage.getItem('cardPoolSFCCommon'));
+		cardPoolSFCUnCommon = JSON.parse(sessionStorage.getItem('cardPoolSFCUnCommon'));
+		cardPoolSFCRareMythic = JSON.parse(sessionStorage.getItem('cardPoolSFCRareMythic'));
+		cardPoolDFC = JSON.parse(sessionStorage.getItem('cardPoolDFC'));
+	}
+	
+	let maxPacks = 0;
+	let numCommons = sessionStorage.getItem('numCommons');
+	let numUnCommons = sessionStorage.getItem('numUnCommons');
+	let numRareMythic = sessionStorage.getItem('numRareMythic');
+	let numDFC = sessionStorage.getItem('numDFC');
 	
 	//Determine max number of packs
 	//Maximum number of packs possible
-	if(sessionStorage.getItem('box') === null) {
-		let maxPacksArr = []
-		maxPacksArr.push(Math.trunc(cardPoolSFCCommon.length/numCommons));
-		maxPacksArr.push(Math.trunc(cardPoolSFCUnCommon.length/numUnCommons));
-		maxPacksArr.push(Math.trunc(cardPoolSFCRareMythic.length/numRareMythic));
-		if(numDFC === null) maxPacksArr.push(Math.trunc(cardPoolDFC.length/numDFC));
-		var maxPacks = Math.min.apply(Math, maxPacksArr);
-		sessionStorage.setItem('maxPacks', JSON.stringify(maxPacks));
+	if(isReset === true) {
+		maxPacks = sessionStorage.getItem('maxPacksOriginal');
 	} else {
-		var maxPacks = JSON.parse(sessionStorage.getItem('maxPacks'));
-	}	
+		maxPacks = JSON.parse(sessionStorage.getItem('maxPacksCounter'));
+	}
+	
+	let box = [];
+	if(sessionStorage.getItem('box') !== null) {
+		box = JSON.parse(sessionStorage.getItem('box'));
+	}
+	
+	//How many packs to make in the box
+	if(maxPacks === 1) {
+		var numPacks = 1;
+	} else if(maxPacks === 0) {
+		return false;
+	} else {
+		var numPacks = num;
+	}
 	
 	//Function that pulls a random card	
-	function randomCard(array) {
-		let i = (Math.random() * array.length);
-		return array.splice(i, 1);
+	function randomCard(pool, pack) {
+		const getRandom = () => (Math.random() * pool.length);
+		let uniqueCards = new Set();
+		let uniqueCard;
+		let unique = false;
+		
+		const hasDuplicates = (card) => {
+			return uniqueCards.size === uniqueCards.add(card.cardName).size;
+		}
+		
+		for (let card of pack) {
+			uniqueCards.add(card.cardName);
+		}
+
+		do {
+			let random = Math.trunc(getRandom());
+			let card = pool[random];
+			
+			if(!hasDuplicates(card)) {
+				pool.splice(random, 1);
+				uniqueCard = card;
+				unique = true;
+			}
+			
+		} while (!unique);
+		
+		return uniqueCard;
+		
 	}
-				
+	
+	//Process the packs			
 	for(let i=0; i < numPacks; i++) {
 		let cardPack = [];
 		
 		//pick our commons
 		for(let i=0; i < numCommons; i++) {
-			//TODO: Make sure that each of these are unique	 to it's pack
-			cardPack.push(randomCard(cardPoolSFCCommon));
+			cardPack.push(randomCard(cardPoolSFCCommon, cardPack));
 		}
 		
 		//pick our uncommons
 		for(let i=0; i < numUnCommons; i++) {
-			//TODO: Make sure that each is unique to it's pack
-			cardPack.push(randomCard(cardPoolSFCUnCommon));
+			cardPack.push(randomCard(cardPoolSFCUnCommon, cardPack));
 		}
 		
 		//pick our rare or mythic
 		for(let i=0; i< numRareMythic; i++) {
-			cardPack.push(randomCard(cardPoolSFCRareMythic));
+			cardPack.push(randomCard(cardPoolSFCRareMythic, cardPack));
 		}
 		
 		//pick our double face card
 		for(let i=0; i< numDFC; i++) {
-			cardPack.push(randomCard(cardPoolDFC));
+			cardPack.push(randomCard(cardPoolDFC, cardPack));
 		}
 		
 		box.push(cardPack);
 	}
 	
+	//Update the pools in the session
+	sessionStorage.setItem('cardPoolSFCCommon', JSON.stringify(cardPoolSFCCommon));
+	sessionStorage.setItem('cardPoolSFCUnCommon', JSON.stringify(cardPoolSFCUnCommon));
+	sessionStorage.setItem('cardPoolSFCRareMythic', JSON.stringify(cardPoolSFCRareMythic));
+	sessionStorage.setItem('cardPoolDFC', JSON.stringify(cardPoolDFC));
+	sessionStorage.setItem('maxPacksCounter', JSON.stringify(maxPacks - numPacks));	
 	sessionStorage.setItem('box', JSON.stringify(box));
 }
 
 function ManaCost(props) {
+
+	function parseCost(cost) {
+		switch(cost) {
+			case 'G':
+				return "Green";
+			case 'R':
+				return "Red";
+			case 'U':
+				return "Blue";
+			case "W":
+				return "White";
+			case "B":
+				return "Black";
+			default:
+				return cost;
+		}
+		
+	}
 	
 	const costs = String(props.manaCost).replace(/[{}]/g, " ").split(" ").filter(n => n);
-	
-	return costs.map((cost, i) => <span className={"mtgicons icon-" + cost} key={i}><span className="path1"></span><span className="path2"></span></span>);
+	return costs.map((cost, i) => <span className={"mtgicons icon-" + cost} key={i}><span className="visually-hidden">{parseCost(cost)}</span><span className="path1"></span><span className="path2"></span></span>);
 }
 
 //React component for our cards
 function Card(props) {
-	
 	const cardClass = [props.colorIdentity, props.rarity];
 	return <li className={cardClass.join(' ')}><span className="name">{props.cardName}</span><ManaCost manaCost={props.manaCost} /></li>;
 }
@@ -209,10 +325,36 @@ function Pack(data, packId) {
 			<div className="pack">
 				<h2>Pack {data.packId+1}</h2>
 				<ul>
-					{cards.map((card, i) => <Card cardName={card[0].cardName} colorIdentity={card[0].colorIdentity} rarity={card[0].rarity} manaCost={card[0].manaCost} key={i}/>)}
+					{cards.map((card, i) => <Card cardName={card.cardName} colorIdentity={card.colorIdentity} rarity={card.rarity} manaCost={card.manaCost} key={i}/>)}
 				</ul>
 			</div>
 	)
+}
+
+function PackNumSelect() {
+	
+	const maxPacks = JSON.parse(sessionStorage.getItem('maxPacksCounter'));
+	if(maxPacks > 0) {
+		let minPacks = 1;
+		let packSelect = Array(maxPacks).fill()
+			.map(() => minPacks++);
+		return (
+			<div className="packSelect">
+				<label htmlFor="packNumSelect">How many packs do you want to generate?</label>
+				<select className="packNumSelect" id="packNumSelect">
+					{packSelect.map((option, i) => <option value={option} key={i}>{option}</option>)} 
+				</select>
+			</div>
+		)
+	} else {
+		return (
+			<div className="packSelect">		
+				<label htmlFor="packNumSelect">How many packs do you want to generate?</label>
+				<select className="packNumSelect" id="packNumSelect" disabled>
+				</select>
+			</div>
+		)
+	}
 }
 
 function Box() {
@@ -222,27 +364,40 @@ function Box() {
 	const [packs, setPacks] = useState([]);
 
 	useEffect(() => {
-		fetch("ISD.json", {method: 'get'})
+		fetch("./ALA.json", {method: 'get'})
 		.then(response => response.json())
 		.then(
 			(result) => {
 				makePools(result);
+				determineMaxPacks();
 		}).then((result) => {
-			 makePacks(6);
 			 setIsLoaded(true);
-			 console.log(JSON.parse(sessionStorage.getItem(('box'))));
-			 setPacks(JSON.parse(sessionStorage.getItem('box')));
 		}).catch(error => {
 			console.log("Error!"); 
 			console.log(error);
 		})
 	}, [])
-	const handleClick = value => () => setSubmitted(value);
+	
+	const submitPackNum = (event) => {
+		event.preventDefault();
+		makePacks(event.target.packNumSelect.value, false);
+		setPacks(JSON.parse(sessionStorage.getItem('box')));
+		setSubmitted(true);
+	}
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		console.log(event);
-		makePacks(event.target.value);
+		makePacks(event.target.more.value, false);
 		setPacks(JSON.parse(sessionStorage.getItem('box')));
+		if(JSON.parse(sessionStorage.getItem('maxPacksCounter') == 0)) {
+			document.getElementById('submit').disabled = true;
+		}
+	}
+	
+	const handleReset = (event) => {
+		event.preventDefault();
+		sessionStorage.removeItem('box');
+		setSubmitted(false);
+		document.getElementById('submit').disabled = false;
 	}
 	
 	if ((!isLoaded) && (!formSubmitted)) {
@@ -260,7 +415,10 @@ function Box() {
 
 					<p>The problem that I ran into is that if I wanted to use my Innistrad cube it would require some amount of set up to best try to recreate an environment that would be like a retail experience, where you open sealed packs of cards. I wrote this script with the intention of being able to randomize my own packs while eventually being able to more replicate a more retail pack-like environment, such as there being no risk of duplicate cards in the pack.</p>
 					
-					<button onClick={handleClick(true)}>Let's go!</button>
+					<form name="selectSet" className="numPacks" onSubmit={submitPackNum}>
+						<PackNumSelect />
+						<button>Let's go!</button>
+					</form>
 				</div>
 			</div>
 		)
@@ -273,10 +431,12 @@ function Box() {
 					</div>
 				</div>
 				<div className="add-more">
-					<form name="morePacks" onSubmit={handleSubmit}>
-						<input type="hidden" name="more" id="more" value="2" />						
-						<button>Make More!</button>
+					<form name="morePacks" onSubmit={handleSubmit} className="packForm">
+						<input type="hidden" name="more" id="more" value="3" />
+						<button type="button" onClick={handleReset} name="reset" id="resetButton">Reset</button>						
+						<button type="submit" name="submit" id="submit">Make More!</button>
 					</form>
+					<span className="remaining">Remaining Packs: {JSON.parse(sessionStorage.getItem('maxPacksCounter'))}</span>
 				</div>
 			</div>
 		)
